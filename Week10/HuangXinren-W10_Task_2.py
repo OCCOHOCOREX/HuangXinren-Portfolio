b75685264c4c045b14bc3e142cdc84ed41d32683
OCCOHOCOREX <64196891+OCCOHOCOREX@users.noreply.github.com>
2021-02-12 15:11:49 +0000

    Delete Week10 directory

--- Week10/HuangXinren-W10_Task_2.py
+++
@@ -1,9 +0,0 @@
-from polygon import Polygon
-
-s = input("how many sides do you want on a dice?")
-d = input("how many dice do you want?")
-t = input("how many times do you want to roll?")
-
-p = polygon(sides = s, length=100)
-
-p.draw()
