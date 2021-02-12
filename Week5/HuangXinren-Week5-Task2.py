a4820affd42707b91dcb88ebb0f21b6f5a76107d
OCCOHOCOREX <64196891+OCCOHOCOREX@users.noreply.github.com>
2021-02-12 15:12:32 +0000

    Delete Week5 directory

--- Week5/HuangXinren-Week5-Task2.py
+++
@@ -1,8 +0,0 @@
-my_dict = {"a":"h", "b":"i", "c":"j", "d":"k", "e":"l", "f":"m", "g":"n", "h":"o", "i":"p", "j":"q" ,"k":"r", "l":"s", "m":"t", "n":"u", "o":"v", "p":"w", "q":"x", "r":"y", "s":"z", "t":"a", "u":"b", "v":"c", "w":"d", "x":"e", "y":"f", "z":"g", " ":" "}
-# sth = input("give a input: ")
-ex = "hello world"
-
-for i in ex:
-    print(my_dict[i])
-
-
